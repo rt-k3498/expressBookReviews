1a689d2f73172d76d71071cae6d38aa24939421e
rt-k3498 <aatharvakawade@gmail.com>
2024-11-02 08:46:50 -0400

    added the modifiucation to the index.js file

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,21 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+   if (req.session.authenticated){
+    let token = req.session.authenticated["access_token"]
+    jwt.verify(token,"secret_key",(err,payload)=>{
+        if (!err){
+            req.payload = payload
+            next()
+        }
+        else{
+            return res.status(403).send({message:"User not authenticated"})
+        }
+    })
+   }
+   else{
+        return res.status(403).send({message:"User not logged in"})
+       }
 });
  
 const PORT =5000;
